b53cbfd41be8a8913c75ba56af7bd80c513c1b8c
Jhonatan de Souza <jdsjhonatan@hotmail.com>
2020-03-20 15:10:58 -0300

    updating login script

--- BotCurtidasInstagram.py
+++ BotCurtidasInstagram.py
@@ -9,23 +9,29 @@ class InstagramBot:
         self.username = username
         self.password = password
         self.driver = webdriver.Firefox(
-            executable_path=r"./Instagram_bots/geckodriver.exe"
+            executable_path=r"./geckodriver.exe"
         )  # Coloque o caminho para o seu geckodriver aqui
 
     def login(self):
         driver = self.driver
         driver.get("https://www.instagram.com")
         time.sleep(3)
-        login_button = driver.find_element_by_xpath(
-            "//a[@href='/accounts/login/?source=auth_switcher']"
-        )
-        login_button.click()
-        user_element = driver.find_element_by_xpath("//input[@name='username']")
+        try:
+            login_button = driver.find_element_by_xpath(
+                "//a[@href='/accounts/login/?source=auth_switcher']"
+            )
+            login_button.click()
+        except:
+            print('já estamos na página de login')
+            pass
+        user_element = driver.find_element_by_xpath(
+            "//input[@name='username']")
         user_element.clear()
         time.sleep(random.randint(4, 6))
         user_element.send_keys(self.username)
         time.sleep(random.randint(4, 6))
-        password_element = driver.find_element_by_xpath("//input[@name='password']")
+        password_element = driver.find_element_by_xpath(
+            "//input[@name='password']")
         password_element.clear()
         password_element.send_keys(self.password)
         time.sleep(random.randint(4, 6))
@@ -50,7 +56,8 @@ class InstagramBot:
         for i in range(
             1, 3
         ):  # Altere o segundo valor aqui para que ele desça a quantidade de páginas que você quiser: quer que ele desça 5 páginas então você deve alterar de range(1,3) para range(1,5)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(3)
         hrefs = driver.find_elements_by_tag_name("a")
         pic_hrefs = [elem.get_attribute("href") for elem in hrefs]
@@ -68,9 +75,11 @@ class InstagramBot:
                 print("pulando link inválido")
                 continue
             driver.get(pic_href)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             try:
-                driver.find_element_by_xpath('//button[@class="dCJp8 afkep"]').click()
+                driver.find_element_by_xpath(
+                    '//button[@class="dCJp8 afkep"]').click()
                 time.sleep(random.randint(19, 23))
             except Exception as e:
                 print(e)
